45e44846f9045a53cf8516774d0a0b74b14efa9a
Ricardo Blanco <rblanco@outlook.com>
2022-11-17 21:35:00 -0600

    update: home view

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -6,4 +6,8 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-export default createVuetify()
+export default createVuetify({
+  theme: {
+    defaultTheme: 'dark',
+  },
+})
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,29 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-const message = ref('')
-const fetchMessage = async () => {
-  const res = await fetch('http://localhost:5000/')
-  const text = await res.text()
-  message.value = text
-}
-fetchMessage()
-</script>
-
 <template>
-  <main>
-    <div>This message is from backend:</div>
-    <h1>{{ message }}</h1>
-  </main>
+  <v-card class="ma-auto pa-4" max-width="60%">
+    <v-list lines="two">
+      <v-list-subheader>Chat</v-list-subheader>
+      <v-list-item>
+        <v-list-title>User</v-list-title>
+        <v-list-item-subtitle>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio
+          aenean sed adipiscing diam donec adipiscing tristique risus nec.
+        </v-list-item-subtitle>
+        <template #prepend>
+          <v-avatar class="mt-auto" color="info">
+            <v-icon icon="mdi-account-circle"></v-icon>
+          </v-avatar>
+        </template>
+      </v-list-item>
+    </v-list>
+    <v-textarea
+      hide-details
+      auto-grow
+      rows="1"
+      label="Mensaje"
+      variant="outlined"
+      append-icon="mdi-send"
+    />
+  </v-card>
 </template>
+<script setup lang="ts"></script>
